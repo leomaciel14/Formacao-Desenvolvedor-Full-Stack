77d27e822a58cfd189c96a5c0e84def8d176eb68
Leonardo Boeira from Windows <leonardoboeira.design@gmail.com>
2024-05-07 16:00:26 -0300

    Atualização 03 07/05 - Envio Atividades 19 e 20

---
+++ Atividade-02/20-Folha-de-Pagamentos.js
@@ -0,0 +1,78 @@
+/// ATIVIDADE 20 ///
+
+/*
+Uma indústria faz a folha mensal de pagamentos de seus 80 empregados baseada no seguinte:
+existe uma tabela com os dados de cada funcionalidade: matrícula, nome e salário bruto.
+Escreva um programa que leia e processe a tabela e emita (escreva na tela), cada funcionário, seu contracheque, cujo formato é dado a seguir:
+Matrícula:
+Nome:
+Salário bruto:
+Dedução INSS:
+Salário líquido:
+(Dicas: desconto de 12%, salário líquido é a diferença entre salário bruto e a redução do INSS).
+*/
+
+const prompt = require('prompt-sync')();
+
+let compilado = [];
+let horas = [];
+let minutos = [];
+let segundos = [];
+
+function checkHoras(x) {
+    let horasTemp = -1;
+    while (horasTemp === -1) {
+        horasTemp = parseInt(prompt(`Digite as horas do ${x + 1}º horário: `));
+        if (horasTemp > 23 || horasTemp < 0 || isNaN(horasTemp)) {
+            console.log('Erro: Informe uma hora que esteja entre 00 e 23 horas.');
+            horasTemp = -1;
+        } else {
+            horas.push(horasTemp);
+        }
+    }
+}
+
+function checkMinutos(x) {
+    let minutosTemp = -1;
+    while (minutosTemp === -1) {
+        minutosTemp = parseInt(prompt(`Digite os minutos do ${x + 1}º horário: `));
+        if (minutosTemp > 59 || minutosTemp < 0 || isNaN(minutosTemp)) {
+            console.log('Erro: Informe minutos que estejam entre 00 e 59.');
+            minutosTemp = -1;
+        } else {
+            minutos.push(minutosTemp);
+        }
+    }
+}
+
+function checkSegundos(x) {
+    let segundosTemp = -1;
+    while (segundosTemp === -1) {
+        segundosTemp = parseInt(prompt(`Digite os segundos do ${x + 1}º horário: `));
+        if (segundosTemp > 59 || segundosTemp < 0 || isNaN(segundosTemp)) {
+            console.log('Erro: Informe segundos que estejam entre 00 e 59.');
+            segundosTemp = -1;
+        } else {
+            segundos.push(segundosTemp);
+        }
+    }
+}
+
+for (let i = 0; i < 5; i++) {
+    checkHoras(i);
+    checkMinutos(i);
+    checkSegundos(i);
+    console.log('');
+    compilado.push([horas[i], minutos[i], segundos[i]]);
+}
+
+console.log('-----------------------------');
+
+function zeroEsquerda(number) {
+    return number.toString().padStart(2, '0');
+}
+
+for (let j = 0; j < compilado.length; j++) {
+    console.log(`Horário ${j + 1}: ${zeroEsquerda(compilado[j][0])}:${zeroEsquerda(compilado[j][1])}:${zeroEsquerda(compilado[j][2])}`);
+    console.log('');
+}
